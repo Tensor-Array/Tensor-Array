08e534af00b7ed830ad8731ef45773165552e718
BigNoobWasTaken <63889503+BigNoobWasTaken@users.noreply.github.com>
2023-12-26 14:05:25 +0700

    test

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/C++_ver2/TensorMain_wsl2
-# Build directory: /mnt/d/C++_ver2/TensorMain_wsl2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/C++_ver2/TensorMain_wsl2/src
-# Build directory: /mnt/d/C++_ver2/TensorMain_wsl2/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
