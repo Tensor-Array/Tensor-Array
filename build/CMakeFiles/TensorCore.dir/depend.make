c56f22c8b0849f52908d1b3d3b4a5f667d0c66d3
BigNoobWasTaken <63889503+BigNoobWasTaken@users.noreply.github.com>
2023-12-26 14:15:42 +0700

    test

---
+++ build/CMakeFiles/TensorCore.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TensorCore.
+# This may be replaced when dependencies are built.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/C++_ver2/TensorMain_wsl2
+# Build directory: /mnt/d/C++_ver2/TensorMain_wsl2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
