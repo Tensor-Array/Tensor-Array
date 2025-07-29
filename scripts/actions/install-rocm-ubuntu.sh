ab42202c4fd95672c226335d516bdb363225f0e9
Noob <63889503+BigNoobWasTaken@users.noreply.github.com>
2025-07-29 07:43:25 +0000

    test

---
+++ scripts/actions/install-rocm-ubuntu.sh
@@ -0,0 +1,115 @@
+ROCM_PACKAGES_IN=(
+    rocm-hip-runtime-devel
+)
+
+function version_ge() {
+    [ "$#" != "2" ] && echo "${FUNCNAME[0]} requires exactly 2 arguments." && exit 1
+    [ "$(printf '%s\n' "$@" | sort -V | head -n 1)" == "$2" ]
+}
+# returns 0 (true) if a > b
+function version_gt() {
+    [ "$#" != "2" ] && echo "${FUNCNAME[0]} requires exactly 2 arguments." && exit 1
+    [ "$1" = "$2" ] && return 1 || version_ge $1 $2
+}
+# returns 0 (true) if a <= b
+function version_le() {
+    [ "$#" != "2" ] && echo "${FUNCNAME[0]} requires exactly 2 arguments." && exit 1
+    [ "$(printf '%s\n' "$@" | sort -V | head -n 1)" == "$1" ]
+}
+# returns 0 (true) if a < b
+function version_lt() {
+    [ "$#" != "2" ] && echo "${FUNCNAME[0]} requires exactly 2 arguments." && exit 1
+    [ "$1" = "$2" ] && return 1 || version_le $1 $2
+}
+
+
+LINUX_ID=$(lsb_release -si)
+LINUX_ID="${LINUX_ID,,}"
+
+LINUX_VERSION=$(lsb_release -sr)
+LINUX_VERSION="${LINUX_VERSION//.}"
+
+LOCATION_TEMP=${temp}
+
+CUDA_VERSION_MAJOR_MINOR=${rocm}
+
+CPU_ARCH=$(uname -m)
+if [[ "${CPU_ARCH}" == "aarch64" ]]
+then
+    CPU_ARCH="sbsa"
+fi
+
+
+ROCM_PACKAGES=""
+for package in "${ROCM_PACKAGES_IN[@]}"
+do : 
+    # Build the full package name and append to the string.
+    ROCM_PACKAGES+=" ${package}-${CUDA_MAJOR}-${CUDA_MINOR}"
+done
+echo "ROCM_PACKAGES ${ROCM_PACKAGES}"
+
+GPG_FILENAME="rocm.gpg.key"
+GPG_URL="https://repo.radeon.com/rocm/${GPG_FILENAME}"
+REPO_URL="https://repo.radeon.com/rocm/apt/${rocm}/"
+
+is_root=false
+if (( $EUID == 0))
+then
+   is_root=true
+fi
+# Find if sudo is available
+has_sudo=false
+if command -v sudo &> /dev/null
+then
+    has_sudo=true
+fi
+# Decide if we can proceed or not (root or sudo is required) and if so store whether sudo should be used or not. 
+if [ "$is_root" = false ] && [ "$has_sudo" = false ]
+then 
+    echo "Root or sudo is required. Aborting."
+    exit 1
+elif [ "$is_root" = false ]
+then
+    USE_SUDO=sudo
+else
+    USE_SUDO=
+fi
+
+if [ -e /etc/apt/keyrings ]
+then
+    $USE_SUDO mkdir --parents --mode=0755 /etc/apt/keyrings
+fi
+
+ROCM_GPG_KEYRING=/etc/apt/keyrings/rocm.gpg
+
+echo "Adding CUDA Repository"
+wget ${GPG_URL} -O - | \
+    gpg --dearmor | $USE_SUDO tee ${ROCM_GPG_KEYRING} > /dev/null
+echo "deb [arch=amd64 signed-by=${ROCM_GPG_KEYRING}] ${REPO_URL} jammy main" \
+    | $USE_SUDO tee /etc/apt/sources.list.d/rocm.list
+echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' \
+    | $USE_SUDO tee /etc/apt/preferences.d/rocm-pin-600
+$USE_SUDO apt-get update
+
+$USE_SUDO apt-get -y install ${ROCM_PACKAGES}
+
+if [[ $? -ne 0 ]]
+then
+    echo "CUDA Installation Error."
+    exit 1
+fi
+
+ROCM_PATH=/opt/rocm-${rocm}
+echo "ROCM_PATH=${ROCM_PATH}"
+export ROCM_PATH=${ROCM_PATH}
+export PATH="$PATH:$ROCM_PATH/bin"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$ROCM_PATH/lib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$ROCM_PATH/lib64"
+
+if [[ $GITHUB_ACTIONS ]]
+then
+    echo "Adding CUDA to CUDA_PATH, PATH and LD_LIBRARY_PATH"
+    echo "${ROCM_PATH}/bin" >> $GITHUB_PATH
+    echo "ROCM_PATH=${ROCM_PATH}" >> $GITHUB_ENV
+    echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" >> $GITHUB_ENV
+fi
