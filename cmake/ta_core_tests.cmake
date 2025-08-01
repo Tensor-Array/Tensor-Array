4578d23f7ee3eb1282b1d675a5719ddbbeec493b
Noob <63889503+BigNoobWasTaken@users.noreply.github.com>
2025-08-01 11:47:27 +0000

    tests

--- cmake/ta_core_tests.cmake
+++ cmake/ta_core_tests.cmake
@@ -1,18 +1,18 @@
 
 set(
     TensorArray_tests_src
-    "tests/tensor-array/core/tensor_array_test.cc"
-    "tests/tensor-array/core/print_output.cc"
-    # "tests/tensor-array/core/tensor_operators.cc"
-    # "tests/tensor-array/core/tensor_matmul_transpose.cc"
-    # "tests/tensor-array/core/gradient.cc"
+    "tensor_array_test.cc"
+    "print_output.cc"
+    # "tensor_operators.cc"
+    # "tensor_matmul_transpose.cc"
+    # "gradient.cc"
     )
 
 enable_testing()
 
 create_test_sourcelist(
     TensorArray_tests
-    "tests/tensor-array/core/test_driver.cc"
+    "test_driver.cc"
     ${TensorArray_tests_src})
 
 add_executable(tensorarray_core_tests ${TensorArray_tests})
@@ -20,6 +20,6 @@ target_include_directories(tensorarray_core_tests PRIVATE ${PROJECT_SOURCE_DIR}/
 target_link_libraries(tensorarray_core_tests TensorArray::Core)
 
 foreach(test ${TensorArray_tests_src})
-    get_filename_component(TName "tests_tensor_array_core_${test}" NAME_WE)
+    get_filename_component(TName ${test} NAME_WE)
     add_test(NAME ${TName} COMMAND tensorarray_core_tests ${TName})
 endforeach()
